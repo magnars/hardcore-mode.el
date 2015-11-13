5ab75594a7a0ca236e2ac87882ee439ff6155d96
Magnar Sveen <magnars@gmail.com>
2015-11-13 18:01:50 +0100

    Release 0.1.0

--- hardcore-mode.el
+++ hardcore-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2011 Magnar Sveen
 
 ;; Author: Magnar Sveen <magnars@gmail.com>
-;; Keywords: marking region
+;; Version: 0.1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
